065ec317710d986c1d048b763aa7f4f852eec260
delometal <delometal@gmail.com>
2022-01-25 18:24:41 +0100

    rename timesheet exc

--- src/main/java/com/perigea/tracker/timesheet/controller/ExceptionHandlerController.java
+++ src/main/java/com/perigea/tracker/timesheet/controller/ExceptionHandlerController.java
@@ -15,7 +15,7 @@ import com.perigea.tracker.timesheet.exception.FestivitaException;
 import com.perigea.tracker.timesheet.exception.GenericError;
 import com.perigea.tracker.timesheet.exception.GruppoException;
 import com.perigea.tracker.timesheet.exception.RuoloException;
-import com.perigea.tracker.timesheet.exception.TimeSheetException;
+import com.perigea.tracker.timesheet.exception.TimesheetException;
 import com.perigea.tracker.timesheet.exception.UtenteException;
 
 @ControllerAdvice
@@ -52,8 +52,8 @@ public class ExceptionHandlerController {
 		return ResponseEntity.badRequest().body(errorDto);
 	}
 
-	@ExceptionHandler(TimeSheetException.class)
-	public final ResponseEntity<?> handleTimeSheetException(TimeSheetException ex){
+	@ExceptionHandler(TimesheetException.class)
+	public final ResponseEntity<?> handleTimeSheetException(TimesheetException ex){
 		GenericError eObject = new GenericError();
 		eObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
 		eObject.setMessage(ex.getMessage());
--- src/main/java/com/perigea/tracker/timesheet/exception/TimesheetException.java
+++ src/main/java/com/perigea/tracker/timesheet/exception/TimesheetException.java
@@ -4,11 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class TimeSheetException extends GenericTimesheetException {
+public class TimesheetException extends GenericTimesheetException {
 
 	private static final long serialVersionUID = -4057094117432592811L;
 
-	public TimeSheetException(String exception) {
+	public TimesheetException(String exception) {
 		super(exception);
 	}
 }

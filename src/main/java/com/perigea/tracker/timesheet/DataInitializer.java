a5cc8a225f158c920bc9b98c6ab3331046388b6d
Massimo Carta <mimmiz91@hotmail.it>
2022-03-07 11:46:09 +0100

    Initializer Cliente If not exist

--- src/main/java/com/perigea/tracker/timesheet/DataInitializer.java
+++ src/main/java/com/perigea/tracker/timesheet/DataInitializer.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Component;
 
 import com.perigea.tracker.commons.enums.FestivitaType;
 import com.perigea.tracker.commons.enums.RuoloType;
+import com.perigea.tracker.commons.utils.Utils;
+import com.perigea.tracker.timesheet.configuration.ApplicationProperties;
+import com.perigea.tracker.timesheet.entity.Cliente;
 import com.perigea.tracker.timesheet.entity.Festivita;
 import com.perigea.tracker.timesheet.entity.Ruolo;
+import com.perigea.tracker.timesheet.repository.ClienteRepository;
 import com.perigea.tracker.timesheet.repository.FestivitaRepository;
 import com.perigea.tracker.timesheet.repository.RuoliRepository;
 
@@ -20,6 +24,12 @@ public class DataInitializer implements ApplicationRunner {
 
 	@Autowired
 	private FestivitaRepository festivitaRepository;
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ApplicationProperties applicationProperties;
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
@@ -36,6 +46,13 @@ public class DataInitializer implements ApplicationRunner {
 			for (FestivitaType f : FestivitaType.values()) {
 				festivitaRepository.save(Festivita.builder().data(f.getData()).nomeFestivo(f).build());
 			}
+		}
+//inizializzazione Cliente con PIva Perigea
+		if(!clienteRepository.findByPartitaIva(applicationProperties.getPartitaIvaPerigea()).isPresent()) {
+			Cliente cliente = new Cliente();
+			cliente.setCodiceAzienda(Utils.uuid());
+			cliente.setPartitaIva(applicationProperties.getPartitaIvaPerigea());
+			clienteRepository.save(cliente);
 		}
 
 	}

605a4cca5a6c9c1e4d257576046bdc2ad8a176e2
samuelgen11 <samuelgen11@gmail.com>
2022-07-13 11:02:53 +0200

    cross origin curriculum controller

--- src/main/java/com/perigea/tracker/timesheet/controller/CurriculumController.java
+++ src/main/java/com/perigea/tracker/timesheet/controller/CurriculumController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import com.perigea.tracker.timesheet.service.FileService;
 
 @RestController
 @RequestMapping("/curriculum")
+@CrossOrigin(allowedHeaders = "*", origins = "*", originPatterns = "*", exposedHeaders = "*")
 public class CurriculumController {
 
 	@Autowired
